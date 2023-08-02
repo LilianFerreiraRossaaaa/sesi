172039aa2c054d2acf457b92af52a475c9ed7484
LilianFerreiraRossaaaa <ferreirarosalilian@gmail.com>
2023-08-02 16:46:13 -0300

    Aula basica getElementById

---
+++ script.js
@@ -0,0 +1,37 @@
+//criar variaveís
+
+//let nome = "Lilian Ferreira"
+let idade = 16
+let idadeIrmão = "16"
+const anoNascimente = 2006 //const = variavel que não irá mudar.
+
+
+// decisão
+
+if (idade === idadeIrmão) {
+alert ("igual...")
+
+}else {
+alert ("cada um no seu quadrado")
+
+}
+
+//lendo informações no HTML    
+
+const titulo = document.getElementById("titulo")
+const nome = document.getElementById("nome")
+const email = document.getElementById()
+
+console.log(titulo)
+
+titulo.textContent = "Mudei o titulo. Eu posso..."
+
+titulo.style.color = "violet"
+titulo.style.backgroundColor = "yellow"
+titulo.style.borderRadius = "10px"
+titulo.style.padding = "10px"
+
+
+function mostrarResultado() {
+
+}
